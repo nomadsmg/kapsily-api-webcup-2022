ccc22221e66c65697a05f7e4f4cbf62bbf47ee62
balzacLeGeek <michaniainar@gmail.com>
2022-05-15 07:05:02 +0300

    Update fixture

--- api/src/DataFixtures/Config/Pricing/PricingFixtureInterface.php
+++ api/src/DataFixtures/Config/Pricing/PricingFixtureInterface.php
@@ -14,20 +14,36 @@ interface PricingFixtureInterface
             'name'       => 'Message audio',
         ],
         2 => [
+            'identifier' => 'offer_picture_message',
+            'name'       => 'Photo',
+        ],
+        3 => [
             'identifier' => 'offer_video_message',
             'name'       => 'Message vidéo',
         ],
-        3 => [
+        4 => [
             'identifier' => 'offer_last_will',
             'name'       => 'Testament',
         ],
-        4 => [
+        5 => [
             'identifier' => 'offer_testimony',
             'name'       => 'Témoignage',
         ],
-        5 => [
+        6 => [
             'identifier' => 'offer_hologramme',
             'name'       => 'Témoignage',
+        ],
+        7 => [
+            'identifier' => 'offer_boomerang',
+            'name'       => 'Boomerang',
+        ],
+        8 => [
+            'identifier' => 'offer_dream_transmission',
+            'name'       => 'Tramission de rêve',
+        ],
+        9 => [
+            'identifier' => 'offer_hologramme',
+            'name'       => 'Hologramme',
         ],
     ];
 
@@ -35,7 +51,7 @@ interface PricingFixtureInterface
         0 => [
             'level'       => 0,
             'identifier'  => 'plan_kapsily_white',
-            'label'       => 'Kapisily fotsy',
+            'label'       => 'Une journée dans votre vie',
             'description' => 'Kapisily fotsy description',
             'minds'       => 0,
             'years'       => 10,
@@ -43,17 +59,17 @@ interface PricingFixtureInterface
         1 => [
             'level'       => 1,
             'identifier'  => 'plan_kapsily_yellow',
-            'label'       => 'Kapisily mavo',
+            'label'       => "L'essentiel",
             'description' => 'Kapisily mavo description',
-            'minds'       => 32,
+            'minds'       => 50,
             'years'       => 50,
         ],
         2 => [
             'level'       => 2,
             'identifier'  => 'plan_kapsily_gold',
-            'label'       => 'Kapisily kely',
+            'label'       => 'Message de la cygone',
             'description' => 'Kapisily kely description',
-            'minds'       => 48,
+            'minds'       => 150,
             'years'       => 1000,
         ],
     ];
@@ -63,13 +79,17 @@ interface PricingFixtureInterface
             'offer_' . self::OFFERS[0]['identifier'],
             'offer_' . self::OFFERS[1]['identifier'],
             'offer_' . self::OFFERS[2]['identifier'],
+            'offer_' . self::OFFERS[3]['identifier'],
         ],
         self::PLANS[1]['identifier'] => [
-            'offer_' . self::OFFERS[3]['identifier'],
             'offer_' . self::OFFERS[4]['identifier'],
+            'offer_' . self::OFFERS[5]['identifier'],
         ],
         self::PLANS[2]['identifier'] => [
-            'offer_' . self::OFFERS[5]['identifier'],
+            'offer_' . self::OFFERS[6]['identifier'],
+            'offer_' . self::OFFERS[7]['identifier'],
+            'offer_' . self::OFFERS[8]['identifier'],
+            'offer_' . self::OFFERS[9]['identifier'],
         ],
     ];
 }
